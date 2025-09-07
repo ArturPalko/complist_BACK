9d5a8abaf188ba4c926b7a2452671d2f30bb890c
Artur Palko <super.gudo32@gmail.com>
2025-09-07 22:23:00 +0300

    Start Gov-Ua page

--- complist_BACK/Program.cs
+++ complist_BACK/Program.cs
@@ -3,19 +3,31 @@ using complist_BACK;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+options.AddPolicy("AllowFrontend", policy =>
+{
+    policy.WithOrigins("http://localhost:3000") 
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+});
+});
+
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 
 
-app.Map("/about", () => "Все працює");
+app.Map("/dovidniki", UserService.GetUsers);
 
 app.UseHttpsRedirection();
 
